8997c51710280f3b02911c8d9d6e9fc7bebc7623
miladsolo1379.8.29@gmail.com <miladsolo1379.8.29@gmail.com>
2023-05-20 17:03:02 +0330

    check conf

--- scripts/run.py
+++ scripts/run.py
@@ -18,6 +18,7 @@ from typing import Dict
 def run(conf: Dict) -> None:
     # Create the save path if it's not exists
     os.makedirs(conf["heatmap_savepath"], exist_ok=True)
+    print(f"{conf['log_save_path'] = }")
 
     if conf["place"] == "outdoor":
         video_process(conf)
